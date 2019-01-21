3e437e93284a173651f66dc84557203f4f466cfa
cjay <680672+cjay@users.noreply.github.com>
2019-01-21 18:41:14 +0100

    Day14 SCC annotations for fastIndexBoxed

--- src/Day14b.hs
+++ src/Day14b.hs
@@ -41,14 +41,15 @@ naiveIndex ix = let
 
 -- fastIndex without the unboxed chunks. same speed as naiveIndex, couldn't find out why
 fastIndexBoxed :: Int -> State ([VL.Vector Word8], VL.Vector (VL.Vector Word8)) Word8
-fastIndexBoxed ix = do
-  (chunks, chunkVec) <- get
-  let (n, i) = ix `divMod` chunkSize
-  if n < VL.length chunkVec then do
+fastIndexBoxed ix = {-# SCC "FIB_all" #-} do
+  (chunks, chunkVec) <- {-# SCC "FIB_get_state" #-} get
+  let (n, i) = {-# SCC "FIB_divMod" #-} ix `divMod` chunkSize
+      len = {-# SCC "FIB_length" #-} VL.length chunkVec
+  {-# SCC "FIB_ifthenelse" #-} if {-# SCC "FIB_n_lt_len" #-} n < len then {-# SCC "FIB_normal_chunk_read" #-} do
     return $ chunkVec VL.! n VL.! i
-  else if n == VL.length chunkVec then do
+  else if {-# SCC "FIB_n_eq_len" #-} n == len then {-# SCC "FIB_head_chunk_read" #-} do
     return $ (head chunks) VL.! i
-  else do
+  else {-# SCC "FIB_vector_append" #-} do
     let (c:chunks') = chunks
         chunkVec' = VL.snoc chunkVec c
     put (chunks', chunkVec')
@@ -71,7 +72,7 @@ fastIndex ix = do
     fastIndex ix
 
 chunks :: [VL.Vector Word8]
-chunks = consume $ evalState (generate fastIndex) (chunks, VL.empty) where
+chunks = consume $ evalState (generate fastIndexBoxed) (chunks, VL.empty) where
 -- chunks = consume $ runIdentity (generate (pure . naiveIndex)) where
   consume xs = let (chunkContent, rest) = explicitSplit chunkSize xs
                    chunk = VL.fromListN chunkSize chunkContent
